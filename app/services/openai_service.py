74e5e86bac9fef609e5380131f3cd4cdecbbbfca
Joseph Heinz <jbheinz14@gmail.com>
2024-10-31 10:09:08 -0500

    tests and comment cleanup
    no functionality change, just cleaned up some of the comments and "manual test" structure of some of the OpenAI services

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -7,42 +7,41 @@ from app.services.openai_completion import return_completion_from_prompt
 from app.services.openai_thread import return_thread_from_prompt
 
 
-# Fetch OpenAI API key from environment variable
-""" openai.api_key = os.getenv("OPENAI_API_KEY")
-if not openai.api_key:
-    raise HTTPException(status_code=500, detail="OpenAI API key not found") """
-
-# Test if the key is loaded
-print(f"Loaded OpenAI API Key: {openai.api_key}")
-
 # Function to call OpenAI API
 def call_openai_api(prompt: str):
     print("Received prompt:")
     print(prompt)
 
+    # check that we have an OpenAI key
+    if openai.api_key is None:
+        raise HTTPException(status_code=500, detail="OpenAI API key not found")
+
     try:
-            
         # two ways to call OpenAPI - completion vs thread
-        # completion is simpler, thread allows more customization and ultimately might be what we want if we are going to open up options
-        #  for the user to provide more inputs about the assistant's response, specify response format, etc
-        openaiPromptType = "completion"  # @TODO toggle this to "completion" for testing if you'd like
-        start_time = time.perf_counter() # stopwatch ON
+        # - completion is simpler, thread allows more customization and ultimately might be what we want if we are going to open up options
+        #   for the user to provide more inputs about the assistant's response, specify response format, etc
+        
+        openaiPromptType = "completion"  # toggle this to "thread" for testing if desired
+        start_time = time.perf_counter() # start stopwatch 
         if openaiPromptType == "completion":
             response = return_completion_from_prompt(prompt)
         else:
             response = return_thread_from_prompt(prompt)
         process_time = time.perf_counter() - start_time  # stopwatch OFF
+        
+        # print processed time
         print("")
         print(f"***Processed OpenAI {openaiPromptType} in {process_time:0.4f} seconds***")
         print("")
 
-        # Print the placeholder response
+        # print response from OpenaI
         print("OpenAI Response:")
-        #print(response_placeholder["choices"][0]["text"])
         print(response)
 
-        return response #response_placeholder
+        return response
         
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error calling OpenAI API: {str(e)}")
     
+if __name__ == "__main__":
+    call_openai_api("hello")
